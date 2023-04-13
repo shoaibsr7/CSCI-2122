b422b7d637a45105db7199087b1096a233ece51c
Ahmed Rajgoli Shoaib Shakeel <69170260+shoaibsr7@users.noreply.github.com>
2023-04-12 23:26:19 -0300

    commit message

---
+++ Lab/Lab3/adjmult/adjmult.h
@@ -0,0 +1,11 @@
+#ifndef __ADJMULT_HEADER
+#define __ADJMULT_HEADER
+
+// A few extra utility functions.
+int imax(int A[], int);
+void readGrid(int G[20][20]);
+
+// Required forward declarations.
+int largestProduct();
+
+#endif
