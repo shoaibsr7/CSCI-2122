b993210fe18cc3f13a98e622977ff47e37f9a637
Ahmed Rajgoli Shoaib Shakeel <69170260+shoaibsr7@users.noreply.github.com>
2023-04-12 23:24:51 -0300

    commit message

---
+++ Lab/Lab1/Challenge2/generate.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#code to make directories
+#counter=1 
+#while [ $counter -le 8 ]
+#do
+#	echo Enter directory name
+#	read file
+#	mkdir $file
+#	((counter++))
+#done
+
+#echo ALL DONE
+
+#directories made
+
+#for value in {1..20}
+#do
+#	echo Enter text for the file
+#	read text
+#	echo Enter file name
+#	read fileName
+#	echo $text > $fileName
+#	echo File created at $fileName
+#done
+
+#echo All done
+mkdir one
+mkdir one/two
+mkdir one/two/three
+mkdir this
+mkdir this/that
+mkdir this/that/theother
+mkdir us
+mkdir us/them
+echo "tell me not to tell me what's for dinner" > one/two/01never  
+echo "these kids at the door" > us/02card
+echo "every puppy you see, but not the flowers" > us/them/03spoil
+echo "decisions make conscious efforts" > this/that/theother/04conscious
+echo "take you out for a steak dinner" > this/05gonna
+echo "all the way to town using only one leg and an expensive hat" > this/that/06bike
+echo "people beatbox their way into my heart" > one/07kindhearted
+echo "the difference between these two pictures" > one/two/three/08spot
+echo "is the first day of your new life as a living thing" > one/two/three/09today
+echo "the lions before you put your head in their mouth" > one/two/three/10tame
+echo "me a hug and buy me dinner because you're making me hungry" > this/that/11give
+echo "rings are expensive things and that's sad" > this/that/12expensive
+echo "him out of the boat and save yourself" > us/them/13boot
+echo "the garden, unless you're scared of flowers" > us/them/14water
+echo "children really test the bounds of child abuse law" > this/15sulky
+echo "should cook dinner for the one you love, especially if it's a puppy" > this/that/theother/16you
+echo "picking flowers and sing me a song about not letting anything go, ever" > one/17start
+echo "ity killed the cat, and that's sad" > one/two/three/18curious
+echo "is boring, unless it's about puppies because puppies are awesome" > one/two/19history
+echo "the creek without paddle will make me late for dinner" > us/them/20up
