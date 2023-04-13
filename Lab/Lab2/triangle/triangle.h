b7c05c1b155c92e01e6114c265acd6d88d91d94e
Ahmed Rajgoli Shoaib Shakeel <69170260+shoaibsr7@users.noreply.github.com>
2023-04-12 23:26:14 -0300

    commit message

---
+++ Lab/Lab2/triangle/triangle.h
@@ -0,0 +1,12 @@
+#ifndef TRIANGLE__HEADER
+#define TRIANGLE__HEADER
+
+void printResult(double, double, double, double, double);
+double findOppositePyth(double, double, double, double);
+double findAdjacentPyth(double, double, double, double);
+double findHypotenusePyth(double, double, double, double);
+double findOppositeTrig(double, double, double, double);
+double findAdjacentTrig(double, double, double, double);
+double findHypotenuseTrig(double, double, double, double);
+
+#endif
