6a13e45a4f33e5547a95f3b5443a631576bfb612
Ahmed Rajgoli Shoaib Shakeel <69170260+shoaibsr7@users.noreply.github.com>
2023-04-12 23:26:22 -0300

    commit message

---
+++ Lab/Lab3/lib/mmath.h
@@ -0,0 +1,12 @@
+#ifndef __MOORE_MATH
+#define __MOORE_MATH
+#include <math.h>
+
+long fact(int);
+long nchoosek(int, int);
+int prime(long);
+int gcd(int, int);
+int lcm(int, int);
+char bitrevc(char);
+
+#endif
