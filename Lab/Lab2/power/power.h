f0176b89efffb8f47a3416e66379ffe96d1f9c28
Ahmed Rajgoli Shoaib Shakeel <69170260+shoaibsr7@users.noreply.github.com>
2023-04-12 23:26:12 -0300

    commit message

---
+++ Lab/Lab2/power/power.h
@@ -0,0 +1,9 @@
+#ifndef __POWER_HEADER
+#define __POWER_HEADER
+
+void powerRule1(int, int);
+void powerRule2(int, int, int);
+void powerRule3(int, int, int, int);
+void powerRule4(int, int, int, int, int);
+
+#endif
